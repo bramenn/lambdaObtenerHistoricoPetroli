09942255ddc4b7a68ee1a575d414a4e9b46d9863
bramenn <bramendev@gmail.com>
2023-10-17 10:43:07 -0500

    Se cambia el codigo incorrecto

--- lambda_function.py
+++ lambda_function.py
@@ -1,37 +1,24 @@
 import os
-from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
 from typing import Dict
 from json import loads
+from bson.json_util import dumps
 
 client = MongoClient(host=os.environ.get("MONGO_URI"))
 
 
 def lambda_handler(event, context):
-    
-    if not event.get("Records"):
-        return "No existen eventos"
-        
-    if not event.get("Records")[0].get("Sns"):
-        return "No se contraron eventos de colas SNS"
-    
-    if event.get("Records")[0].get("Sns").get("Type") != "Notification":
-        return "No es un evento de notificación"
-        
-    if not event.get("Records")[0].get("Sns").get("Message"):
-        return "Ningun mensaje encontrado"
-    
-    
-    
-    data: Dict = loads(event.get("Records")[0].get("Sns").get("Message"))
 
+    numero_registros = event.get("numero_datos", 100)
+    query = event.get("query")
+    
 
     db = client.petrolera
 
     collection = db.eventos_activos_petroleros
 
-    result = collection.insert_one(data)
 
-    if result.inserted_id:
-        return "Document inserted successfully"
-    else:
-        return "Failed to insert document"
+    result = collection.find(query).limit(int(numero_registros)).sort([("timestamp", DESCENDING)])
+    
+        
+    return loads(dumps(result))
